24322ca7c2a7e235e5a3d20ce03bfd56c0064940
faazizpro <fahadafs11@gmail.com>
2022-09-29 18:04:13 +0600

    Desktop version done

--- src/components/Calculate/Calculate.js
+++ src/components/Calculate/Calculate.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 import './Calculate.css'
 import person from '../../images/person.jpg'
-const Calculate = ({time}) => {
+import { addToDb } from '../../utilities/fakedb';
+const Calculate = (props) => {
+    const [breakTime, setBreakTimes] = useState(0);
+    const times = props.time;
+    console.log(times);
+    const allTimes = times.reduce((p, c) => p + c.time, 0);
+
+    const handleTime = (e) => {
+        const value = e.currentTarget.value;
+        setBreakTimes(value);
+
+        addToDb(value)
+    }
+
+    
+
     return (
         <div>
             <div className='flex justify-center bg-orange-500 w-3/4 mx-auto p-5 rounded-xl mt-3 text-white'>
-            <div className="person-image-name flex gap-11">
-                <img className='w-16 rounded-lg' src={person} alt="" />
-                <div className="info">
-                    <h2 className='text-3xl font-semibold'>Robert Henry</h2>
-                    <h2 className='text-lg font-medium'><FontAwesomeIcon icon={faLocationDot} className='mr-2'></FontAwesomeIcon>London, UK</h2>
+                <div className="person-image-name flex gap-11">
+                    <img className='w-16 rounded-lg' src={person} alt="" />
+                    <div className="info">
+                        <h2 className='text-3xl font-semibold'>Robert Henry</h2>
+                        <h2 className='text-lg font-medium'><FontAwesomeIcon icon={faLocationDot} className='mr-2'></FontAwesomeIcon>London, UK</h2>
+                    </div>
                 </div>
-            </div>
             </div>
 
             <div className='body-measure bg-orange-500 flex gap-20 mt-5 p-5 w-3/4 mx-auto justify-center rounded-xl text-white'>
@@ -34,24 +49,24 @@ const Calculate = ({time}) => {
 
             {/* Add A Break Section Starts */}
             <h2 className='text-3xl text-center mt-5 font-bold underline'>Add a Break</h2>
-            <div className='break-time bg-orange-500 flex gap-10 mt-5 p-5 w-3/4 mx-auto justify-center rounded-xl'> 
+            <div className='break-time bg-orange-500 flex gap-10 mt-5 p-5 w-3/4 mx-auto justify-center rounded-xl'>
                 <div>
-                    <button className='bg-white text-black p-3 font-bold text-2xl rounded-full hover:bg-slate-900 hover:text-yellow-200  cursor-pointer'>
+                    <button onClick={handleTime} value="5" className='bg-white text-black p-3 font-bold text-2xl rounded-full hover:bg-slate-900 hover:text-yellow-200  cursor-pointer'>
                         5m
                     </button>
                 </div>
                 <div>
-                    <button className='bg-white text-black p-3 font-bold text-2xl rounded-full hover:bg-slate-900 hover:text-yellow-200  cursor-pointer'>
+                    <button onClick={handleTime} value="10" className='bg-white text-black p-3 font-bold text-2xl rounded-full hover:bg-slate-900 hover:text-yellow-200  cursor-pointer'>
                         10m
                     </button>
                 </div>
                 <div>
-                    <button className='bg-white text-black p-3 font-bold text-2xl rounded-full hover:bg-slate-900 hover:text-yellow-200  cursor-pointer'>
+                    <button onClick={handleTime} value="15" className='bg-white text-black p-3 font-bold text-2xl rounded-full hover:bg-slate-900 hover:text-yellow-200  cursor-pointer'>
                         15m
                     </button>
                 </div>
                 <div>
-                    <button className='bg-white text-black p-3 font-bold text-2xl rounded-full hover:bg-slate-900 hover:text-yellow-200  cursor-pointer'>
+                    <button onClick={handleTime} value="20" className='bg-white text-black p-3 font-bold text-2xl rounded-full hover:bg-slate-900 hover:text-yellow-200  cursor-pointer'>
                         20m
                     </button>
                 </div>
@@ -62,19 +77,19 @@ const Calculate = ({time}) => {
             <h2 className='text-3xl text-center mt-10 font-bold underline'>Exercise Details</h2>
             <div className='exercise-detail bg-orange-500 flex gap-10 mt-5 p-5 w-11/12 mx-auto  rounded-xl'>
                 <div className='exercise-time bg-white p-2 w-3/4 rounded-xl'>
-                    <h2 className='font-bold text-xl'>Exercise Time: {time.length} min</h2>
+                    <h2 className='font-bold text-xl'>Exercise Time: {allTimes} min</h2>
                 </div>
 
                 <div className='break-time bg-white p-2 w-4/5 rounded-xl'>
-                    <h2 className='font-bold text-xl'>Break Time: </h2>
+                    <h2 className='font-bold text-xl'>Break Time: {breakTime}</h2>
                 </div>
             </div>
             {/* Exercise Details Section End */}
 
             {/* Activity Completed Section Starts */}
-                <div className='flex justify-center'>
-                    <button className='bg-orange-500 py-3 px-10 rounded-lg font-bold text-xl text-white mt-7'>Activity Completed</button>
-                </div>
+            <div className='flex justify-center'>
+                <button className='bg-orange-500 py-3 px-10 rounded-lg font-bold text-xl text-white mt-7'>Activity Completed</button>
+            </div>
             {/* Activity Completed Section Ends */}
         </div>
     );
--- src/components/GYM/GYM.js
+++ src/components/GYM/GYM.js
@@ -23,22 +23,46 @@ const GYM = () => {
     }
 
     return (
-        <div className='gym-container'>
-            <div className="exercise-container grid grid-cols-3 gap-10 mt-14 ml-14 mb-14">
-                {
-                    exercises.map(exercise => <Exercise
-                    key={exercise.id}
-                    exercise={exercise}
-                    handleAddToList={handleAddToList}
-                    ></Exercise>)
-                }
-            </div>
+        <div >
+            <div className='gym-container'>
+                <div className="exercise-container grid grid-cols-3 gap-10 mt-14 ml-14 mb-14">
+                    {
+                        exercises.map(exercise => <Exercise
+                        key={exercise.id}
+                        exercise={exercise}
+                        handleAddToList={handleAddToList}
+                        ></Exercise>)
+                    }
+                </div>
 
-            <div className="calculate-container">
-                <h2 className='text-2xl'>Total Time in calculate component</h2>
-                <Calculate
-                    time={time}
-                ></Calculate>
+                <div className="calculate-container">
+                    <h2 className='text-2xl'>Total Time in calculate component</h2>
+                    <Calculate
+                        time={time}
+                    ></Calculate>
+                </div>
+            </div>
+            <div className='blog'>
+                    <h2 className='text-4xl font-bold text-center mb-5 text-orange-500'>Our Blog Section</h2>
+                <div className='w-2/4'>
+                    <h2 className='text-3xl font-semibold ml-10'>Q1: How Does React Work</h2>
+                    <p className='text-lg font-semibold ml-10 mb-16 text-justify'><span className='text-2xl font-bold'>Ans:</span> ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+
+                    <h2 className='text-3xl font-semibold ml-10'>Q2: What is The Difference Between props and state?</h2>
+                    <p className='text-lg font-semibold ml-10 mb-16 text-justify'><span className='text-2xl font-bold'>Ans:</span> Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component</p>
+
+                    <h2 className='text-3xl font-semibold ml-10'>Q3: Where useEffects is used except data load?</h2>
+                    <p className='text-lg font-semibold ml-10 mb-16 text-justify'><span className='text-2xl font-bold'>Ans:</span> We will learn six usages of useEffect hook to run and clean up a side effect. <br />
+                    <li>Side Effect Runs After Every Render</li>
+                    <li>Side Effect Runs Only Once After Initial Render</li>
+                    <li>Side Effect Runs After State Value Changes</li>
+                    <li>Side Effect Runs After Props Value Change</li>
+                    <li>Side Effect Runs After Props and State Value Change</li>
+                    <li>Side EffectCleanup</li>
+                    
+                    </p>
+                </div>
+ 
             </div>
         </div>
     );
